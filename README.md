bf0509c3974d3b6deed0a28bd6a2563d58038295
materesszer <materesszer2@gmail.com>
2025-08-21 12:50:28 +0200

    init project initial project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Minecraft Mod Downloader Tool
+
+This tools scans your mod folder, and searches for the newest version of them on modrinth and downloads them.
+
+## Recommended IDE Setup
+
+[VS Code](https://code.visualstudio.com/) + [Svelte](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer).
+
+## How to run:
+  cd mcmoddownloadtool
+  npm install
+  npm run tauri android init
+
+### For Desktop development, run:
+  npm run tauri dev
+
+### For Android development, run:
+  npm run tauri android dev
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+let files: FileList | null = null;
+</script>
+
+
+
+<main class="container">
+  <h1>Minecraft mod downloader tool</h1>
+
+  <div class="row"></div>
+      <img src="/mc512.png" class="logo mc" alt="Minecraft Logo" />
+
+  <h2>Select your mod folder</h2>
+  <input bind:files id="dir" multiple type="file" webkitdirectory>
+
+  {#if files}
+  <table>
+    <thead>
+      <tr>
+        <td>Name</td>
+        <td>Size</td>
+      </tr>
+    </thead>
+    <tbody>
+  {#each Array.from(files ? Array.from(files) : []) as file}
+    <tr>
+    <td>{file.name}</td>
+    <td>{file.size}</td>
+    </tr>
+  {/each}
+  </tbody>
+  </table>
+  {/if}
+  
+
+
+</main>
+
+<style>
+
+
+</style>
